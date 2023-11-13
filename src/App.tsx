76c7c94499d8b06ffac634621c234e5697fa17ce
Mariano De Luca <m.deluca.virtualspace@gmail.com>
2023-11-13 15:56:29 +0100

    *

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { AiOutlineCoffee } from 'react-icons/ai'; 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
